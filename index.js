b4f319c99346258a8a096aa70bde9fa33e82dabe
Denisolo <deniisolo@outlook.es>
2023-11-13 18:08:39 -0500

    preguntas en js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function starboton(){
     alert("Bienvenido")
+    document.getElementById("preguntas1").style.display = 'block';
+    document.getElementById("start-form").style.display = 'none';
+    document.getElementById("final").style.display = 'none';
 }
+function buttonnext(){
+    alert("buenaaa")
+    document.getElementById("preguntas2").style.display = 'block';
+    document.getElementById("preguntas1").style.display = 'none';
+}
+
+function buttonnext2(){
+    alert("buenaaa 2")
+    document.getElementById("preguntas3").style.display = 'block';
+    document.getElementById("preguntas2").style.display = 'none';
+}
+function buttonnext3(){
+    alert("buenaaa 3")
+    document.getElementById("final").style.display = 'block';
+    document.getElementById("preguntas3").style.display = 'none';
+}
+
+
