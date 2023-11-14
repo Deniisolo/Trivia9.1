f156c32b814f1de441650d6118c1c14a9db749d7
Denisolo <deniisolo@outlook.es>
2023-11-14 12:52:01 -0500

    estructura trivia

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function starboton(){
     alert("Bienvenido")
+    document.getElementById('cronometro').style.display = 'block';
     document.getElementById("preguntas1").style.display = 'block';
     document.getElementById("start-form").style.display = 'none';
     document.getElementById("final").style.display = 'none';
@@ -19,3 +20,18 @@ function buttonnext3(){
     document.getElementById("final").style.display = 'block';
     document.getElementById("preguntas3").style.display = 'none';
 }
+
+function starCronometro() {
+    let tiempoRestante = 10; 
+    const cronometroElemento = document.getElementById('cronometro');
+    const intervalo = setInterval(function () {
+        if (tiempoRestante <= 0) {
+            clearInterval(intervalo);
+            alert('¡Tiempo agotado!');
+        } else {
+            cronometroElemento.textContent = tiempoRestante;
+            tiempoRestante--;
+        }
+    }, 10000);
+}
+starCronometro()
